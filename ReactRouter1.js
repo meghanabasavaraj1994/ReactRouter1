f1fa181980124b5dc039f1410abf1144846c569a
meghanabasavaraj1994 <102538632+meghanabasavaraj1994@users.noreply.github.com>
2022-03-29 22:48:52 +0530

    Add files via upload

---
+++ ReactRouter1.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Clothes from "./Clothes";
+
+function App2() {
+  return (
+    <Fragment>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/Mens">Men's</Link>
+            </li>
+            <li>
+              <Link to="/Ladies">Ladies</Link>
+            </li>
+          </ul>
+        </div>
+
+        <Switch>
+          <Route path="/:type">
+            <Clothes />
+          </Route>
+        </Switch>
+      </Router>
+    </Fragment>
+  );
+}
+
+export default App2;
